53f048a7985c27ee3c0e91a60e8047169b3d65ce
Piotr Jackowski <pjackowski18@gmail.com>
2021-03-04 03:17:14 -0600

    created models for our db

---
+++ src/server/models.py
@@ -0,0 +1,35 @@
+from flask_mongoengine import MongoEngine, connection
+import datetime
+from mongoengine.fields import EmbeddedDocumentField
+from mongoengine.queryset.base import CASCADE
+
+
+
+from server import db
+import mongoengine as me
+db:MongoEngine
+
+class Grouping(me.Document):
+    name = me.StringField(required = True)
+    description = me.StringField()
+
+
+class FollowingConnections(me.EmbeddedDocument):
+    following_id = me.ReferenceField('Account')
+    distance     = me.IntField()
+
+class Account(me.Document):
+    twitter_handle = me.StringField(required = True)
+    name           = me.StringField()
+    update_date    = me.DateField(default=datetime.datetime.utcnow)
+    group_type     = me.ReferenceField(Grouping)
+    connections    = me.ListField(EmbeddedDocumentField(FollowingConnections))
+
+
+
+
+
+class Search(me.Document):
+    searched_id = me.ReferenceField(Account, required=True, reverse_delete_rule=CASCADE,)
+    search_handle = me.StringField()
+    connections = me.ListField(EmbeddedDocumentField(FollowingConnections))
