8338b999a0e009cf6b74ae6451def172166e8d6c
Piotr Jackowski <pjackowski18@gmail.com>
2021-03-06 02:56:18 -0600

    improvement to naming in database

--- src/server/models.py
+++ src/server/models.py
@@ -9,20 +9,20 @@ from mongoengine.queryset.base import CASCADE
 import mongoengine as me
 db:MongoEngine
 
-class Grouping(me.Document):
+class Group(me.Document):
     name = me.StringField(required = True)
     description = me.StringField()
 
 
 class FollowingConnections(me.EmbeddedDocument):
-    following_id = me.ReferenceField('Account')
-    distance     = me.IntField()
+    following = me.ReferenceField('Account')
+    distance  = me.IntField()
 
 class Account(me.Document):
     twitter_handle = me.StringField(required = True)
     name           = me.StringField()
     update_date    = me.DateField(default=datetime.datetime.utcnow)
-    group_type     = me.ReferenceField(Grouping)
+    group_type     = me.ReferenceField(Group)
     connections    = me.ListField(EmbeddedDocumentField(FollowingConnections))
 
 
@@ -30,6 +30,5 @@ class Account(me.Document):
 
 
 class Search(me.Document):
-    searched_id = me.ReferenceField(Account, required=True, reverse_delete_rule=CASCADE,)
-    search_handle = me.StringField()
+    search_handle = me.ReferenceField(Account, required=True, reverse_delete_rule=CASCADE)
     connections = me.ListField(EmbeddedDocumentField(FollowingConnections))
