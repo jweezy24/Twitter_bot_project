6b0df64f005cade0dbe3f1fdd6dc3af6416589e4
Jack <jwest1@luc.edu>
2021-03-09 14:23:07 -0600

    added a word ranking algorithm, it picks n amount of most used words from a dictionary

--- src/twitter_bot/categorization_algorithm.py
+++ src/twitter_bot/categorization_algorithm.py
@@ -19,11 +19,55 @@ def filter_out_words(data):
         pos_sentences=[nltk.pos_tag(sent) for sent in tokenized_sent]
         for sentence in pos_sentences:
             for word,w_type in sentence:
-                if (word,w_type) in out:
-                    out[(word,w_type)] = out[(word,w_type)] + 1
-                else:
-                    out[(word,w_type)] = 1
-
+                if w_type in types_of_words_to_filter and len(word) > 1:
+                    if (word.lower(),w_type) in out:
+                        out[(word.lower(),w_type)] = out[(word.lower(),w_type)] + 1
+                    else:
+                        out[(word.lower(),w_type)] = 1
     return out
+
+'''
+This method will rank each word found in a given dictionary.
+First, we sort each word by useage.
+input:
+    data = dictionary of words where the indexs are the word and type as the method above would create.
+    total = the amount of imprant words the user would like to see.
+output:
+    out = a list of those words ranked
+'''
+def rank_words_dictionary(data, total=50):
+    #Creates a list of empty lists
+    lst = [[] for i in range(0,total)]
+    #Create a copy of the dictionary given such that we can alter the dictionary. 
+
+    totals = list(data.values())
+    totals.sort()
+    totals.reverse()
+
+    mappings = totals[0:total]
+
+    #For our searching algorithm we are going to use a dynamic programing approach    
+    for ind in data.keys():
+        if data[ind] in mappings:
+            tmp_ind = mappings.index(data[ind])
+            
+            #if the mapping is already filled i.e the amount has already been used in that position we recursively find the next index.
+            def rec_check(ind, lst):
+                if ind >= len(lst):
+                    return None
+                if lst[ind] == []:
+                    return ind
+                else:
+                    return rec_check(ind+1, lst)
+            new_ind = rec_check(tmp_ind, lst)
+            
+            if new_ind != None:
+                lst[new_ind] = (ind[0], ind[1], data[ind])
+
+
+    print(lst)
+    return lst
+        
+        
 
 
--- tests/categorization_tests.py
+++ tests/categorization_tests.py
@@ -13,5 +13,12 @@ class TestLanguageFunctions(unittest.TestCase):
         self.assertTrue(type(output) == type({}))
         self.assertTrue(len(output.keys()) != 0)
 
+    def test_rank_words_dictionary(self):
+        favorites = get_all_favorites('alittl3ton13',table="favorite_tbl")
+        output = filter_out_words(favorites)
+        ranked = rank_words_dictionary(output)
+        self.assertTrue(type(ranked) == type([]))
+        self.assertTrue(len(ranked) != 0)
+
 if __name__ == "__main__":
     unittest.main()
