13f4359651b86c7cd38cf50b49017c0642759e9c
Piotr Jackowski <pjackowski18@gmail.com>
2021-03-11 00:27:53 -0600

    finished controller

--- src/server/db_controller.py
+++ src/server/db_controller.py
@@ -1,15 +1,26 @@
+from re import search
 import mongoengine
 import datetime
 from server.models import *
 
 
+#return account if it exists else return None
+def get_account(twitter_handle:str):
+    try:
+        return Account.objects(twitter_handle = twitter_handle).get()
+    except mongoengine.errors.DoesNotExist:
+        return None
+
 def insert_account(data:dict):
     
-    account = Account(twitter_handle=data['twitter_handle'])
+    account = get_account(data['twitter_handle'])
+    if account is None:
+        account = Account(twitter_handle=data['twitter_handle'])
+    
     account.name = data['name']
     account.update_date = datetime.datetime.utcnow()
     # check if group_type is present and if not in database insert it 
-    # not sure if this is what we whant though maybe we donr want it to add a new group
+    # not sure if this is what we whant though maybe we dont want it to add a new group
     if "group_type" in data:
         group = get_group(data['group_type'])
         if group is not None:
@@ -17,13 +28,23 @@ def insert_account(data:dict):
         else:
             insert_group({'name':data['group_type']})
             account.group_type = get_group(data['group_type'])
+    #insert connections after creating them if account already has connections we create a search to store the old search
     if "connections" in data:
+        if len(account.connections) > 0: 
+            create_search(account)
+        account.connections.clear()
         for connection in data['connections']:
             account.connections.append(generate_following_connection(connection))
     
     account.save()
+        
     return account
 
+def update_account(account:Account, data:dict):
+    account.name = data['name']
+    account.update_date = datetime.datetime.utcnow()
+
+#add the group into the database and returns if it worksTODO
 def insert_group(data:dict):
     group = Group(name = data['name'])
     if  'description' in data:
@@ -31,10 +52,12 @@ def insert_group(data:dict):
     group.save()
     return group
 
+#insert an list of accounts
 def insert_accounts(data:list):
     for account in data:
         insert_account(account)
 
+#return group if it exists else return None
 def get_group(name:str):
     try:
         return Group.objects(name = name).get()
@@ -42,6 +65,7 @@ def get_group(name:str):
     except mongoengine.errors.DoesNotExist:
         return None
 
+#used by insert account to create the following connections if it doesnt exist it inserts the account this is recursive
 def generate_following_connection(data:dict):
     connection = FollowingConnections()
     following = get_account(data['following']['twitter_handle'])
@@ -53,11 +77,15 @@ def generate_following_connection(data:dict):
     connection.distance = data['distance']
     return connection
 
-    
-    
+def create_search(account:Account):
+    search = Search(search_handle = account)
+    search.connections = account.connections
+    search.date = account.update_date
+    search.save()
 
-def get_account(twitter_handle:str):
+def find_previous_search(twitter_handle:str):
     try:
-        return Account.objects(twitter_handle = twitter_handle).get()
+        account = get_account(twitter_handle)
+        return Search.objects(search_handle = account).get()
     except mongoengine.errors.DoesNotExist:
         return None
