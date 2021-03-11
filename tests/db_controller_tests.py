d6db5e9e549eefecb29289d6eaf255b2c0115016
Piotr Jackowski <pjackowski18@gmail.com>
2021-03-11 00:28:13 -0600

    tests for the rest of the controller

--- tests/db_controller_tests.py
+++ tests/db_controller_tests.py
@@ -2,8 +2,7 @@ import unittest
 from mongoengine import connect, disconnect
 import sys
 
-from mongoengine.queryset.transform import query
-from mongoengine.queryset.visitor import Q
+
 #sys.path.append('.')
 sys.path.append('./src')
 #sys.path.insert(1, '../src/server/')
@@ -20,6 +19,8 @@ class TestController(unittest.TestCase):
         disconnect()
         connect('mongoenginetest', host="mongomock://localhost")
 
+        
+
     @classmethod
     def tearDownClass(cls):
         disconnect()
@@ -33,7 +34,7 @@ class TestController(unittest.TestCase):
 
         assert query.name == group['name']
 
-    def test_insert_account(self):
+    def test_create_account(self):
         controller.insert_account({"twitter_handle":"John21", "name" :"John Doe"})
 
         actual = models.Account.objects(twitter_handle = "John21").first()
@@ -70,3 +71,47 @@ class TestController(unittest.TestCase):
         query2 = models.Account.objects(twitter_handle = "Jane6").get()
 
         assert query.connections[0].following.twitter_handle == "Jane6"
+    
+    #insert account or update if it exists
+    def test_insert_account_update(self):
+        account = {"twitter_handle":"Jane7", "name" :"Jane Doe", 'group_type':'Politics', 
+        "connections": [{"following":{"twitter_handle":"Jane6", "name" :"Jane Doe", 'group_type':'Politics'},'distance':10}]}
+        
+        controller.insert_account(account)
+
+        account['name'] = 'Jane Smith'
+        account['connections'].clear()
+        account['connections'].append({"following":{"twitter_handle":"Jane4", "name" :"Jane Doe", 'group_type':'Politics'},'distance':5})
+
+        controller.insert_account(account)
+
+        query = models.Account.objects(twitter_handle = "Jane7").get()
+
+        assert query.name == "Jane Smith"
+        assert query.connections[0].following.twitter_handle == "Jane4"
+
+    def test_find_previous_search(self):
+        account = controller.insert_account({"twitter_handle":"John4", "name" :"John Doe", 'group_type':'Politics'})
+        connection = controller.generate_following_connection({"following":{"twitter_handle":"Jane6", "name" :"Jane Doe", 'group_type':'Politics'},'distance':10})
+        
+        search = models.Search(search_handle = account)
+        search.connections.append(connection)
+        search.date = account.update_date
+        print(search.date)
+        search.save()
+
+        query = controller.find_previous_search(account.twitter_handle)
+        print(query.date)
+        assert query != None
+        
+        assert query.connections[0].following.twitter_handle == "Jane6"
+
+    def test_create_search(self):
+        account = controller.insert_account({"twitter_handle":"John55", "name" :"John Doe", 'group_type':'Politics', "connections": [{"following":{"twitter_handle":"Jane6", "name" :"Jane Doe", 'group_type':'Politics'},'distance':10}]})
+        
+        controller.create_search(account)
+
+        query = models.Search.objects(search_handle = account).get()
+
+        assert query.search_handle.twitter_handle == account.twitter_handle
+    
