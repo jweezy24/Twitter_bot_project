ad9e13e39f298cf534c6dbf094d1449107807e2a
Piotr Jackowski <pjackowski18@gmail.com>
2021-03-13 22:46:53 -0600

    added route to get about and graphs

--- clientSide-server/flaskServer/routes.py
+++ clientSide-server/flaskServer/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from flaskServer import app, db
 
 @app.route("/")
@@ -6,3 +6,15 @@ from flaskServer import app, db
 def home():
     
     return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/graph", methods=['GET'])
+def graph():
+    twitterHandle = request.args['TwitterHandleInput']
+    print(twitterHandle)
+    data = twitterHandle
+    #return data and load the page with the graph
+    return render_template("graph.html", data = data)
