a200e26f3de81a4ca6f3c2c7d9a5e8e96185b67e
Jack West <jackwest245@gmail.com>
2021-04-16 12:00:00 -0500

    migrated mongo into the data algorithm

--- src/run.py
+++ src/run.py
@@ -1,4 +1,13 @@
+import argparse
 from server import app
+
+parser = argparse.ArgumentParser(description='Main appliction controller.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
 
 if __name__ == '__main__':  
     app.run(debug=True)
