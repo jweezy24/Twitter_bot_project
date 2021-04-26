aa962789a23a5288af97d31162483d69095b54c6
Piotr Jackowski <pjackowski18@gmail.com>
2021-04-26 16:25:12 -0500

    removing mongo from front end

--- clientSide-server/flaskServer/__init__.py
+++ clientSide-server/flaskServer/__init__.py
@@ -1,15 +1,10 @@
 from flask import Flask
-from flask_mongoengine import MongoEngine
+
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'TwitterBotDB' ##needs different name
-    ##username and password if needed
-    ##host : ip,
-    ##port : num
-}
 
-db = MongoEngine(app)
+
+
 
 from flaskServer import routes
